3b28e360c575bd034a9bf4a67bec5ea5cc12f8f9
Sahil Barethiya <sahilbarethiya885@gmail.com>
2025-08-09 15:24:14 +0530

    Initial commit

---
+++ premier-fantasy/src/main/java/com/plf/premier_fantasy/player/PlayerRepository.java
@@ -0,0 +1,15 @@
+package com.plf.premier_fantasy.player;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player,String>{
+
+    void deleteByName(String playerName);
+
+    Optional<Player> findByName(String name);
+    
+}
---
+++ premier-fantasy/src/test/java/com/plf/premier_fantasy/PremierFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plf.premier_fantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
